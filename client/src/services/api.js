5b9fccdce4c0f07cc672fe0d2e439590479d620e
Sujal-Dawande25 <sujaldawande@gmail.com>
2026-03-25 11:07:54 +0530

    connected frontend to live backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
     import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://resume-analyzer-45kb.onrender.com"
 });
 
 export const uploadResume = (formData) => API.post("/upload", formData);
